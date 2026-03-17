ea85e846fd92db244dbd8acbfcbd807256118d26
deadmanoz <hones02_tunica@icloud.com>
2026-03-17 21:28:02 +0800

    chore: release v0.6.0

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
         {
           peer-observer-agent = pkgs.rustPlatform.buildRustPackage {
             pname = "peer-observer-agent";
-            version = "0.5.3";
+            version = "0.6.0";
             src = ./.;
             cargoLock.lockFile = ./Cargo.lock;
 
